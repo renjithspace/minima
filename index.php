5ba77e82cb6798444bca1a0176ffd2826cacb5b8
renjithspace <renjithrajeevvk@gmail.com>
2018-01-08 19:06:28 +0530

    Base version

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php if (is_home()): ?>
+        <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
+    <?php else: ?>
+        <title><?php the_title(); ?></title>
+    <?php endif; ?>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css">
+</head>
+<body <?php body_class(); ?>>
+
+    <header>
+        <a href="<?php bloginfo('url'); ?>" class="name"><?php bloginfo('name'); ?></a>
+        <p><?php bloginfo('description'); ?></p>
+    </header>
+
+    <main>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php static $count = 0; if($count == 1) {break;}?>
+            <section>
+                <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+                <div class="meta">
+                    <span class="time"><?php the_time('jS F Y'); ?></span>
+                </div>
+                <div class="content">
+                    <?php wp_trim_words( the_content(), 55, "" )?>
+                </div>
+            </section>
+            <footer>
+                <ul>
+                    <li><?php previous_post_link( '%link', '%title', true ); ?></li>
+                    <li><?php next_post_link( '%link', '%title', true ); ?></li>
+                </ul>
+            </footer>
+        <?php $count++; ?>
+    <?php endwhile; endif; ?>
+    </main>
+
+</body>
+</html>
