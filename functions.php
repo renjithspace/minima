36ab8404c45fc81c2dc1135e4ae6a3bc0ac87ba2
renjithspace <renjithrajeevvk@gmail.com>
2018-01-09 16:45:33 +0530

    Add read time calculate

--- functions.php
+++ functions.php
@@ -1,1 +1,17 @@
 <?php
+
+// Reading time calculate
+function the_readtime($postID) {
+    $content = get_post_field( 'post_content', $postID );
+    $word_count = str_word_count( strip_tags( $content ) );
+    $readingtime = ceil($word_count / 200);
+
+    if ($readingtime == 1) {
+        $timer = " minute";
+    } else {
+        $timer = " minutes";
+    }
+    $totalreadingtime = $readingtime . $timer;
+
+    return $totalreadingtime;
+}
