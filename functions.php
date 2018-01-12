e1b351a37b35ca20680db5e4afaab69fb18264d6
renjithspace <renjithrajeevvk@gmail.com>
2018-01-12 22:02:27 +0530

    Add description parser

--- functions.php
+++ functions.php
@@ -21,3 +21,10 @@ function the_category_name($postID) {
     $category = get_the_category($postID);
     echo $category[0]->cat_name;
 }
+
+// Description
+function the_description($postID) {
+    $content = get_post_field( 'post_content', $postID );
+    $description = substr( strip_tags($content), 0, 320);
+    echo $description;
+}
