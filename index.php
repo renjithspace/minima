19a766d92d8490b37ac555f4710e8968ba560f44
renjithspace <renjithrajeevvk@gmail.com>
2018-01-12 21:44:16 +0530

    Include header and footer

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <?php if (is_home()): ?>
-        <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
-    <?php else: ?>
-        <title><?php the_title(); ?></title>
-    <?php endif; ?>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css">
-</head>
-<body <?php body_class(); ?>>
+    <?php get_header(); ?>
 
     <header>
         <a href="<?php bloginfo('url'); ?>" class="name"><?php bloginfo('name'); ?></a>
@@ -43,9 +30,6 @@
     <?php endwhile; endif; ?>
     </main>
 
-    <footer>
-        <p>&copy; <?php bloginfo('site'); ?> <?php echo date('Y'); ?></p>
-    </footer>
-
+    <?php get_footer(); ?>
 </body>
 </html>
