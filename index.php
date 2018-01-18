3d84927153b26f2ccb71f35247fbd009b8930d26
renjithspace <renjithrajeevvk@gmail.com>
2018-01-18 19:42:16 +0530

    Update the posts wrapper tag as article

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
 
 <main>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <section>
+    <article>
         <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
         <div class="content">
             <p><?php the_short_content(get_the_ID()); ?></p>
         </div>
-    </section>
+    </article>
 <?php endwhile; endif; ?>
 </main>
 
