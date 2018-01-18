5e0eb81e3ac4bfd822627883481cc67e8f056071
renjithspace <renjithrajeevvk@gmail.com>
2018-01-18 19:33:40 +0530

    List all posts

--- index.php
+++ index.php
@@ -7,21 +7,12 @@
 
 <main>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <?php static $count = 0; if($count == 1) {break;}?>
-        <section>
-            <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
-            <div class="content">
-                <p><?php the_description(get_the_ID()); ?></p>
-            </div>
-        </section>
-
-        <nav>
-            <ul>
-                <li><?php previous_post_link( '%link', '%title', true ); ?></li>
-                <li><?php next_post_link( '%link', '%title', true ); ?></li>
-            </ul>
-        </nav>
-    <?php $count++; ?>
+    <section>
+        <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+        <div class="content">
+            <p><?php the_description(get_the_ID()); ?></p>
+        </div>
+    </section>
 <?php endwhile; endif; ?>
 </main>
 
